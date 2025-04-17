b1ddad1ec7b5394ce69d2f44974482fa8acbb205
avigaildanesh <avigaildanesh100@gmail.com>
2025-04-17 16:09:17 +0300

    cities changed

--- src/utils/cities.js
+++ src/utils/cities.js
@@ -5,5 +5,10 @@ export const CITIES = [
     { name: "Bangkok, Thailand",   lat: 13.7563, lon: 100.5018 },
     { name: "Tel Aviv, Israel",    lat: 32.0853, lon: 34.7818 },
     { name: "Tokyo, Japan",        lat: 35.6895, lon: 139.6917 },
+    // { name: "Toronto, Canada",     lat: 43.651070, lon: -79.347015 },
+    // { name: "Oymyakon, Russia",    lat: 63.4641, lon: 142.7737 },
+    // { name: "Yakutsk, Russia",     lat: 62.0355, lon: 129.6755 },
+    // { name: "Tongulakh, Russia",   lat: 63.5753, lon: 121.6360 },
+    // { name: "Mohe, China",         lat: 52.9723, lon: 122.5385 },
   ];
   
